55eaa25f570e2042469e6f71ff0bec1ce5760fbf
SaratAngajalaoffl <saratangajala@gmail.com>
2021-04-13 01:40:25 +0530

    Added functionality and email validation for student

--- routes/institute.js
+++ routes/institute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const InstituteModel = require('../models/institute');
+const StudentModel = require('../models/student');
 
 const Router = express.Router();
 
@@ -17,6 +18,19 @@ Router.post('/', (req, res, next) => {
 	const hash = bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
 
 	const doc = new InstituteModel({ name, password: hash });
+	doc.save((err, newit) => {
+		if (err) next(err);
+		else {
+			res.status(201);
+			res.setHeader('Content-Type', 'application/json');
+			res.send(newit);
+		}
+	});
+});
+
+Router.post('/add-student', (req, res, next) => {
+	const { email, id } = req.body;
+	const doc = new StudentModel({ email });
 	doc.save((err, newit) => {
 		if (err) next(err);
 		else {
