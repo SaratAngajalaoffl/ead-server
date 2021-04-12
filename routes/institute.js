08628e5d3475c3bc758e0baa51c8457fd38b1a56
SaratAngajalaoffl <saratangajala@gmail.com>
2021-04-13 01:28:12 +0530

    Added code for password hashing using bcrypt and removed password from the get response

--- routes/institute.js
+++ routes/institute.js
@@ -1,17 +1,22 @@
 const express = require('express');
-
+const bcrypt = require('bcrypt');
 const InstituteModel = require('../models/institute');
+
 const Router = express.Router();
 
 Router.get('/', async (req, res, next) => {
-	const query = await InstituteModel.find({});
+	const query = await InstituteModel.find({}).select(['-password', '-__v']);
 	res.status(200);
 	res.setHeader('Content-Type', 'application/json');
 	res.send(query);
 });
 
 Router.post('/', (req, res, next) => {
-	const doc = new InstituteModel(req.body);
+	const { name, password } = req.body;
+
+	const hash = bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
+
+	const doc = new InstituteModel({ name, password: hash });
 	doc.save((err, newit) => {
 		if (err) next(err);
 		else {
@@ -20,6 +25,13 @@ Router.post('/', (req, res, next) => {
 			res.send(newit);
 		}
 	});
+});
+
+Router.post('/login', async (req, res, next) => {
+	const { name, password } = req.body;
+	const query = await InstituteModel.findOne({ name });
+
+	res.send(bcrypt.compareSync(password, query.password));
 });
 
 module.exports = Router;
